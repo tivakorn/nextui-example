0b257f3e0178f689673454492bc3eec81a48286a
tivakorn <not_not_not1@hotmail.co.th>
2022-09-08 01:46:34 +0700

    :lipstick: Update theme color.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-/** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+const withPWA = require('next-pwa')({ dest: 'public' })
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true
 }
 
 module.exports = withPWA(nextConfig)
